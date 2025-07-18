b5b99a23b95f31f2c0c237d5b42ac82254d94f4b
Deniz1968 <ddenizcetinn@outlook.com>
2025-07-18 20:43:50 +0300

    Initial Commit

---
+++ cccc/ccccApp.swift
@@ -0,0 +1,17 @@
+//
+//  ccccApp.swift
+//  cccc
+//
+//  Created by Deniz Cetin on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ccccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ccccTests/ccccTests.swift
@@ -0,0 +1,17 @@
+//
+//  ccccTests.swift
+//  ccccTests
+//
+//  Created by Deniz Cetin on 18/07/2025.
+//
+
+import Testing
+@testable import cccc
+
+struct ccccTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
